cdaae227c47d406662607b59f23bd17a0d87f02f
Xing Ern <lxingern@gmail.com>
2023-02-13 18:21:55 +0800

    implement ability to increase/decrease cart item amount

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -8,7 +8,7 @@ const MealItemForm = (props) => {
     
     const submitHandler = (event) => {
         event.preventDefault()
-        const enteredAmount = amountInputRef.current.defaultValue
+        const enteredAmount = amountInputRef.current.value
         const enteredAmountNumber = +enteredAmount
         
         if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
